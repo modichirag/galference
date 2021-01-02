005a6135aba2462e03fa33678e52063f0ec3496d
modichirag <modichirag@berkeley.edu>
2021-01-01 20:24:55 -0800

    correct activation in parallel

---
+++ code/rim/cosmic_rim_multi_parallel.py
@@ -0,0 +1,365 @@
+""" Implementation of Cosmic RIM estimator"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+import os, sys, argparse, time
+from scipy.interpolate import InterpolatedUnivariateSpline as iuspline
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+
+from rim_utils import build_rim_parallel, build_rim, myAdam
+import flowpm
+from flowpm import linear_field, lpt_init, nbody, cic_paint
+from flowpm.utils import r2c3d, c2r3d
+sys.path.append('../../utils/')
+import tools
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    print("Name:", gpu.name, "  Type:", gpu.device_type)
+
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+    
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--nc', type=int, default=16, help='Grid size')
+parser.add_argument('--bs', type=float, default=100, help='Box Size')
+parser.add_argument('--nsteps', type=int, default=3, help='')
+parser.add_argument('--niter', type=int, default=200, help='Number of iterations/Max iterations')
+parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
+parser.add_argument('--optimizer', type=str, default='adam', help='Which optimizer to use')
+parser.add_argument('--batch_size', type=int, default=8, help='Batch size')
+parser.add_argument('--nsims', type=int, default=100, help='Batch size')
+parser.add_argument('--nbody', type=str2bool, default=True, help='Number of simulationss')
+parser.add_argument('--lpt_order', type=int, default=1, help='Order of LPT Initial conditions')
+parser.add_argument('--input_size', type=int, default=8, help='Input layer channel size')
+parser.add_argument('--cell_size', type=int, default=8, help='Cell channel size')
+parser.add_argument('--rim_iter', type=int, default=10, help='Optimization iteration')
+parser.add_argument('--epochs', type=int, default=10, help='Number of epochs')
+parser.add_argument('--suffix', type=str, default='', help='Suffix for folder pathname')
+parser.add_argument('--batch_in_epoch', type=int, default=20, help='Number of batches in epochs')
+
+
+
+args = parser.parse_args()
+
+
+nc, bs = args.nc, args.bs
+niter = args.niter
+optimizer = args.optimizer
+lr = args.lr
+a0, af, nsteps = 0.1, 1.0,  args.nsteps
+stages = np.linspace(a0, af, nsteps, endpoint=True)
+#anneal = True
+#RRs = [2, 1, 0.5, 0]
+
+#
+klin = np.loadtxt('../../data/Planck15_a1p00.txt').T[0]
+plin = np.loadtxt('../../data//Planck15_a1p00.txt').T[1]
+ipklin = iuspline(klin, plin)
+# Compute necessary Fourier kernels                                                                                                                          
+kvec = tools.fftk((nc, nc, nc), boxsize=nc, symmetric=False)
+kmesh = (sum(k**2 for k in kvec)**0.5).astype(np.float32)
+priorwt = ipklin(kmesh)
+
+
+#RIM params
+params = {}
+params['input_size'] = args.input_size
+params['cell_size'] = args.cell_size
+params['strides'] = 2
+params['middle_size'] = args.input_size // params['strides']  #lets divide by strides
+params['cell_kernel_size'] = 5
+params['input_kernel_size'] = 5
+params['middle_kernel_size'] = 5
+params['output_kernel_size'] = 5
+params['rim_iter'] = args.rim_iter
+params['input_activation'] = 'tanh'
+params['output_activation'] = 'linear'
+params['nc'] = nc
+params['batch_size'] = args.batch_size
+params['epoch'] = args.epochs
+
+
+
+
+def get_ps(iterand, truth):
+    ic, fin = truth
+    ic1, fin1 = iterand
+
+    pks = []
+    if abs(ic1[0].mean()) < 1e-3: ic1[0] += 1
+    #if abs(ic[0].mean()) < 1e-3: ic[0] += 1                                                                                                                  
+    k, p1 = tools.power(ic1[0]+1, boxsize=bs)
+    k, p2 = tools.power(ic[0]+1, boxsize=bs)
+    k, p12 = tools.power(ic1[0]+1, f2=ic[0]+1, boxsize=bs)
+    pks.append([p1, p2, p12])
+    if fin1[0].mean() < 1e-3: fin1[0] += 1
+    if fin[0].mean() < 1e-3: fin[0] += 1
+    k, p1 = tools.power(fin1[0], boxsize=bs)
+    k, p2 = tools.power(fin[0], boxsize=bs)
+    k, p12 = tools.power(fin1[0], f2=fin[0], boxsize=bs)
+    pks.append([p1, p2, p12])
+
+    return k, pks
+
+
+
+
+    
+def get_data(nsims=args.nsims):
+    if args.nbody: dpath = '../../data/rim-data/L%04d_N%03d_T%02d/'%(bs, nc, nsteps)
+    else: dpath = '/project/projectdirs/m3058/chmodi/rim-data/L%04d_N%03d_LPT%d/'%(bs, nc, args.lpt_order)
+    print(dpath)
+    alldata = np.array([np.load(dpath + '%04d.npy'%i) for i in range(nsims)]).astype(np.float32)
+    traindata, testdata = alldata[:int(0.9*nsims)], alldata[int(0.9*nsims):]
+    return traindata, testdata
+    
+
+@tf.function()
+def pm_data(dummy):
+    print("PM graph")
+    linear = flowpm.linear_field(nc, bs, ipklin, batch_size=args.batch_size)
+    if args.nbody:
+        print('Nobdy sim')
+        state = lpt_init(linear, a0=a0, order=args.lpt_order)
+        final_state = nbody(state,  stages, nc)
+    else:
+        print('ZA/2LPT sim')
+        final_state = lpt_init(linear, a0=af, order=args.lpt_order)
+    tfinal_field = cic_paint(tf.zeros_like(linear), final_state[0])
+    return linear, tfinal_field
+
+@tf.function
+def pm(linear):
+    if args.nbody:
+        print('Nobdy sim')
+        state = lpt_init(linear, a0=a0, order=args.lpt_order)
+        final_state = nbody(state,  stages, nc)
+    else:
+        print('ZA/2LPT sim')
+        final_state = lpt_init(linear, a0=af, order=args.lpt_order)
+    tfinal_field = cic_paint(tf.zeros_like(linear), final_state[0])
+    return tfinal_field
+
+
+
+@tf.function
+def recon_dm(linear, data):
+    """                                                                                                                                                   
+    """
+    print('new graph')
+    final_field = pm(linear)
+    residual = final_field - data
+
+    chisq = tf.multiply(residual, residual)
+    chisq = tf.reduce_mean(chisq)
+#     chisq = tf.multiply(chisq, 1/nc**3, name='chisq')
+
+    #Prior
+    lineark = r2c3d(linear, norm=nc**3)
+    priormesh = tf.square(tf.cast(tf.abs(lineark), tf.float32))
+    prior = tf.reduce_mean(tf.multiply(priormesh, 1/priorwt))
+#     prior = tf.multiply(prior, 1/nc**3, name='prior')
+    #                                                                                                                                                     
+    loss = chisq + prior
+
+    return loss, chisq, prior
+
+
+@tf.function
+def recon_dm_grad(x, y):
+    with tf.GradientTape() as tape:
+        tape.watch(x)
+        loss = recon_dm(x, y)[0]
+    grad = tape.gradient(loss, x)
+    return grad
+
+
+#strategy = tf.distribute.MirroredStrategy(devices=["/device:GPU:0", "/device:GPU:1"])
+strategy = tf.distribute.MirroredStrategy()
+print ('Number of devices: {}'.format(strategy.num_replicas_in_sync))
+
+#traindata, testdata = get_data()
+#print(traindata.shape, testdata.shape)
+
+#BUFFER_SIZE = len(traindata)
+BATCH_SIZE_PER_REPLICA = params['batch_size']
+GLOBAL_BATCH_SIZE = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
+EPOCHS = params['epoch']
+#train_dataset = tf.data.Dataset.from_tensor_slices((traindata[:, 0], traindata[:, 1])).shuffle(BUFFER_SIZE).batch(GLOBAL_BATCH_SIZE) 
+#test_dataset = tf.data.Dataset.from_tensor_slices((testdata[:, 0], testdata[:, 1])).batch(strategy.num_replicas_in_sync) 
+
+train_dataset = tf.data.Dataset.range(args.batch_in_epoch)
+train_dataset = train_dataset.map(pm_data)
+# dset = dset.apply(tf.data.experimental.unbatch())
+train_dataset = train_dataset.prefetch(-1)
+test_dataset = train_dataset
+
+train_dist_dataset = strategy.experimental_distribute_dataset(train_dataset)
+test_dist_dataset = strategy.experimental_distribute_dataset(test_dataset)
+
+# Create a checkpoint directory to store the checkpoints.
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+
+
+with strategy.scope():
+
+    #rim = (params)
+    #rim = build_rim(params)
+    rim = build_rim_parallel(params)
+    grad_fn = recon_dm_grad
+
+    def get_opt(lr):
+        return  tf.keras.optimizers.Adam(learning_rate=lr)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=args.lr)
+    checkpoint = tf.train.Checkpoint(model=rim)
+    #
+
+
+
+def train_step(inputs):
+    x_true, y = inputs
+    x_init = tf.random.normal(x_true.shape)
+    with tf.GradientTape() as tape:
+        x_pred = rim(x_init, y, grad_fn)
+        res  = (x_true - x_pred)
+        loss = tf.reduce_mean(tf.square(res)) ##This is not advised, come back to this
+    gradients = tape.gradient(loss, rim.trainable_variables)
+    #optimizer = get_opt(lr)
+    optimizer.apply_gradients(zip(gradients, rim.trainable_variables))
+    return loss
+
+
+def test_step(inputs):
+    x_true, y = inputs
+    x_init = tf.random.normal(x_true.shape)
+    x_pred = rim(x_init, y, grad_fn)
+    return x_pred, x_init, x_true, y
+
+
+
+# `run` replicates the provided computation and runs it
+# with the distributed input.
+@tf.function
+def distributed_train_step(dataset_inputs):
+  per_replica_losses = strategy.run(train_step, args=(dataset_inputs,))
+  return strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_losses,
+                         axis=None)
+
+@tf.function
+def distributed_test_step(dataset_inputs):
+    return strategy.run(test_step, args=(dataset_inputs,))
+
+
+##
+
+losses = []    
+
+for x in test_dist_dataset:
+    a, b, c, d = distributed_test_step(x)
+    a, b, c, d = a.values[0], b.values[0], c.values[0], d.values[0]
+    #test_callback(a.numpy()[-1], b.numpy(), c.numpy(), d.numpy(), suff="-init", pref='models/L%04d_N%03d_T%02d/'%(bs, nc, nsteps))
+    break
+
+
+adam = myAdam(params['rim_iter'])
+adam10 = myAdam(10*params['rim_iter'])
+
+suffpath = '_parallel' + args.suffix
+if args.nbody: ofolder = './models/L%04d_N%03d_T%02d%s/'%(bs, nc, nsteps, suffpath)
+else: ofolder = './models/L%04d_N%03d_LPT%d%s/'%(bs, nc, args.lpt_order, suffpath)
+try: os.makedirs(ofolder)
+except Exception as e: print(e)
+print("\nOutput folder : ", ofolder, "\n")
+
+for epoch in range(EPOCHS):
+    print("\nFor epoch %d\n"%epoch)
+    #TRAIN LOOP
+    total_loss = 0.0
+    num_batches = 0
+    starte = time.time()
+    for x in train_dist_dataset:
+        print(num_batches)
+        #print(x[0].values[0].shape)
+        startb = time.time()
+        loss = distributed_train_step(x)
+        losses.append(loss.numpy())
+        total_loss += loss
+        print("epoch %d, num batch %d, loss : "%(epoch, num_batches), loss)
+        print("Time taken : ", time.time() - startb)
+        num_batches += 1
+    train_loss = total_loss / num_batches
+    print("Train loss for epoch %d "%epoch, train_loss)
+    print("Time taken for epoch %d: "%epoch, time.time() - starte)
+    plt.plot(losses)
+    plt.savefig(ofolder + 'losses.png')
+
+    ##Test Epoch Training
+    for x in test_dist_dataset:
+        a, b, c, d = distributed_test_step(x)
+        pred, x_init, xx, yy = a.values[0], b.values[0], c.values[0], d.values[0]
+        pred = pred[-1]
+        pred_adam = adam(x_init, yy, grad_fn)
+        pred_adam10 = adam10(x_init, yy, grad_fn)
+        
+        fig, ax = plt.subplots(1, 3, figsize = (12, 4))
+        vmin, vmax = xx[0].numpy().sum(axis=0).min(), xx[0].numpy().sum(axis=0).max()
+        ax[0].imshow(xx[0].numpy().sum(axis=0), vmin=vmin, vmax=vmax)
+        ax[0].set_title('Truth')
+        ax[1].imshow(x_init[0].numpy().sum(axis=0), vmin=vmin, vmax=vmax)
+        ax[1].set_title('initial point')
+        ax[2].imshow(pred[0].numpy().sum(axis=0), vmin=vmin, vmax=vmax)
+        ax[2].set_title('RIM %d step'%(params['rim_iter']))
+        plt.savefig(ofolder + 'rim-im-%d.png'%epoch)
+        plt.close()
+
+        ##
+        fig, ax = plt.subplots(1, 2, figsize=(9, 4))
+        #print(x_init.shape, xx.shape, yy.shape, pred.shape, pred_adam.shape, pred_adam10.shape)
+        k, pks = get_ps([x_init.numpy(), pm(x_init).numpy()], [xx.numpy(), yy.numpy()])
+        for i in range(2):
+            ax[0].plot(k, pks[i][2]/(pks[i][0]*pks[i][1])**0.5, 'C%d--'%i, lw=0.5)
+            ax[1].plot(k, (pks[i][0]/pks[i][1])**0.5, 'C%d--'%i, lw=0.5)
+
+        k, pks = get_ps([pred.numpy(), pm(pred).numpy()], [xx.numpy(), yy.numpy()])
+        for i in range(2):
+            ax[0].plot(k, pks[i][2]/(pks[i][0]*pks[i][1])**0.5, 'C%d'%i)
+            ax[1].plot(k, (pks[i][0]/pks[i][1])**0.5, 'C%d'%i)
+
+        k, pks = get_ps([pred_adam.numpy(), pm(pred_adam).numpy()], [xx.numpy(), yy.numpy()])
+        for i in range(2):
+            ax[0].plot(k, pks[i][2]/(pks[i][0]*pks[i][1])**0.5, 'C%d-.'%i, lw=0.5)
+            ax[1].plot(k, (pks[i][0]/pks[i][1])**0.5, 'C%d-.'%i, lw=0.5)
+
+        k, pks = get_ps([pred_adam10.numpy(), pm(pred_adam10).numpy()], [xx.numpy(), yy.numpy()])
+        for i in range(2):
+            ax[0].plot(k, pks[i][2]/(pks[i][0]*pks[i][1])**0.5, 'C%d:'%i)
+            ax[1].plot(k, (pks[i][0]/pks[i][1])**0.5, 'C%d:'%i)
+
+        for axis in ax: 
+            axis.semilogx()
+            axis.grid(which='both')
+        plt.savefig(ofolder + 'rim-2pt-%d.png'%epoch)
+        plt.close()
+
+        break
+
+    rim.save_weights(ofolder + '/%d'%epoch)
+    
+    
+
+#    
--- code/rim/rim_utils.py
+++ code/rim/rim_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 from convolutional_recurrent import ConvLSTM3DCell
-from tensorflow.python.keras.layers import Conv3D, Conv3DTranspose
+from tensorflow.python.keras.layers import Conv3D, Conv3DTranspose, MaxPool3D, AveragePooling3D
 
 import sys
 sys.path.append('../../utils/')
@@ -61,68 +61,22 @@ class RIM3D(tf.keras.Model):
 
 
 
+def build_rim(params):
 
+    nc = params['nc']
+    input_layer = Conv3D(params['input_size'], kernel_size=params['input_kernel_size'], 
+                         trainable=True, padding='SAME',
+                         input_shape=(None, nc, nc, nc, 2), activation=params['input_activation'])
 
-class RIM3D_series(tf.keras.Model):
+    cell = ConvLSTM3DCell(params['cell_size'], kernel_size=params['cell_kernel_size'], padding='SAME')
+    cell.build(input_shape=[None, nc, nc, nc, params['input_size']])
 
-    def __init__(self, cell1, cell2, input_layer, middle_layer, output_layer, strides, niter):
-        super(RIM3D_series, self).__init__()
-        self.cell1 = cell1
-        self.cell2 = cell2
-        self.output_layer = output_layer
-        self.middle_layer = middle_layer
-        self.input_layer = input_layer
-        self.strides = strides
-        self.niter = niter
-        self.beta_1, self.beta_2 = 0.9, 0.999
-        self.lr, self.eps = 0.1, 1e-7
-            
-            
-    def call(self, x_init, y, grad_fn, grad_args=[], initstates = None, return_steps=False):
-        
-        outputs_ta = tf.TensorArray(size=self.niter+1, dtype=tf.float32)
-        
-        if initstates is None: 
-            #stateshape = tuple(i//self.strides for i in x_init.shape) + tuple([self.cell1.filters])
-            nc2 = int(x_init.shape[1]/self.strides)
-            stateshape = (x_init.shape[0], nc2, nc2, nc2, self.cell1.filters)
-            initstates1 = [tf.zeros(stateshape), tf.zeros(stateshape)]
-            stateshape = x_init.shape + tuple([self.cell2.filters])
-            initstates2 = [tf.zeros(stateshape), tf.zeros(stateshape)]
-            initstates = [initstates1, initstates2]
+    output_layer = Conv3D(1, kernel_size=params['output_kernel_size'], trainable=True, padding='SAME', 
+                          input_shape=(None, nc, nc, nc, params['cell_size']), activation=params['output_activation'])
+   
+    rim = RIM3D(cell, input_layer, output_layer, niter=params['rim_iter'])
 
-        i = tf.constant(0, dtype=tf.int32)
-        curr_state = initstates
-        curr_pos = x_init        
-        m = tf.zeros_like(x_init)
-        v = tf.zeros_like(x_init)
-        
-        def body(i, pos, states, m, v):  
-            gradient = grad_fn(pos, y, *grad_args)           
-            t = tf.cast(i+1, tf.float32)
-            m = self.beta_1*m + (1-self.beta_1)*gradient
-            v = self.beta_2*v + (1-self.beta_2)*gradient**2
-            mc = m/(1-self.beta_1**t)
-            vc = v/(1-self.beta_2**t)
-            delta = -1.*self.lr*mc/(tf.sqrt(vc) + self.eps)
-
-            states1, states2 = states
-            concat_input = tf.stack([pos, delta], axis=-1)
-            cell_input = self.input_layer(concat_input)            
-            delta_pos, new_states1 = self.cell1(cell_input, states1)
-            delta_pos = self.middle_layer(delta_pos)
-            delta_pos, new_states2 = self.cell2(delta_pos, states2)
-            delta_pos = self.output_layer(delta_pos)[...,0]
-            new_pos = pos + delta_pos
-            new_states = [new_states1, new_states2]
-            return i +1 , new_pos, new_states, m, v
-        
-        while tf.less(i, tf.constant(self.niter)):
-            outputs_ta = outputs_ta.write(i, curr_pos)
-            i, curr_pos, curr_state, m, v =  body(i, curr_pos, curr_state, m, v)
-        outputs_ta = outputs_ta.write(i, curr_pos)
-        return outputs_ta.stack()
-
+    return rim
 
 
 class RIM3D_parallel(tf.keras.Model):
@@ -142,11 +96,14 @@ class RIM3D_parallel(tf.keras.Model):
             
             
     def call(self, x_init, y, grad_fn, grad_args=[], initstates = None, return_steps=False):
-        
+
         outputs_ta = tf.TensorArray(size=self.niter+1, dtype=tf.float32)
+
         
         if initstates is None: 
             #stateshape = tuple(i//self.strides for i in x_init.shape) + tuple([self.cell1.filters])
+            #stateshape = x_init.shape + tuple([self.cell.filters])
+            #initstates = [tf.zeros(stateshape), tf.zeros(stateshape)]
             nc2 = int(x_init.shape[1]/self.strides)
             stateshape = (x_init.shape[0], nc2, nc2, nc2, self.cell1.filters)
             initstates1 = [tf.zeros(stateshape), tf.zeros(stateshape)]
@@ -174,22 +131,59 @@ class RIM3D_parallel(tf.keras.Model):
             #
             cell_input_sub = self.input_layer_sub(concat_input)
             delta_pos1, new_states1 = self.cell1(cell_input_sub, states1)
-            delta_pos1 = self.output_layer_up(delta_pos1)
+            delta_pos1 = self.output_layer_up(delta_pos1) 
             #
             cell_input = self.input_layer(concat_input)
             delta_pos2, new_states2 = self.cell2(cell_input, states2)
-            delta_pos2 = self.output_layer(delta_pos2)
+            #delta_pos2 = self.output_layer(delta_pos2)
             #
-            delta_pos = delta_pos1 + delta_pos2
+            #delta_pos = delta_pos1 + delta_pos2
+            delta_pos = tf.concat([delta_pos1, delta_pos2], axis=-1)
+            delta_pos = self.output_layer(delta_pos)
             new_pos = pos + delta_pos[..., 0]
-            new_states = [new_states1, new_states2]
+            #new_states = [new_states1, new_states2]
+            new_states = [states1, new_states2]
             return i +1 , new_pos, new_states, m, v
-        
+
+
         while tf.less(i, tf.constant(self.niter)):
             outputs_ta = outputs_ta.write(i, curr_pos)
             i, curr_pos, curr_state, m, v =  body(i, curr_pos, curr_state, m, v)
         outputs_ta = outputs_ta.write(i, curr_pos)
         return outputs_ta.stack()
+
+
+
+
+def build_rim_parallel(params):
+
+    nc = params['nc']
+    input_layer = Conv3D(params['input_size'], kernel_size=params['input_kernel_size'], 
+                         trainable=True, padding='SAME', 
+                         input_shape=(None, nc, nc, nc, 2), activation=params['input_activation'])
+
+    input_layer_sub = Conv3D(params['input_size'], kernel_size=params['input_kernel_size'], 
+                             trainable=True, padding='SAME', strides= [params['strides']]*3,
+                             input_shape=(None, nc, nc, nc, 2), activation=params['input_activation'])
+
+    #input_layer_sub = MaxPool3D(padding='SAME')
+    #input_layer_sub = AveragePooling3D(padding='SAME')
+    cell1 = ConvLSTM3DCell(params['cell_size'], kernel_size=params['cell_kernel_size'], padding='SAME')
+    #cell1.build(input_shape=[None, nc, nc, nc, params['input_size']])
+
+    output_layer_up = Conv3DTranspose(params['cell_size'], kernel_size=params['middle_kernel_size'], 
+                         trainable=True, padding='SAME', strides=[params['strides']]*3, 
+                         activation=params['output_activation'])
+
+    cell2 = ConvLSTM3DCell(params['cell_size'], kernel_size=params['cell_kernel_size'], padding='SAME')
+
+    output_layer = Conv3D(1, kernel_size=params['output_kernel_size'], trainable=True, padding='SAME', 
+                          input_shape=(None, nc, nc, nc, params['cell_size']*2), activation=params['output_activation'])
+   
+    rim = RIM3D_parallel(cell1, cell2, input_layer, input_layer_sub, output_layer_up, output_layer, strides=params['strides'],
+                       niter=params['rim_iter'])
+
+    return rim
 
 
 class myAdam(tf.keras.Model):
@@ -234,25 +228,9 @@ class myAdam(tf.keras.Model):
         return curr_pos
 
 
-def build_rim(params):
-
-    nc = params['nc']
-    input_layer = Conv3D(params['input_size'], kernel_size=params['input_kernel_size'], 
-                         trainable=True, padding='SAME',
-                         input_shape=(None, nc, nc, nc, 2), activation=params['input_activation'])
-
-    cell = ConvLSTM3DCell(params['cell_size'], kernel_size=params['cell_kernel_size'], padding='SAME')
-    cell.build(input_shape=[None, nc, nc, nc, params['input_size']])
-
-    output_layer = Conv3D(1, kernel_size=params['output_kernel_size'], trainable=True, padding='SAME', 
-                          input_shape=(None, nc, nc, nc, params['cell_size']), activation=params['output_activation'])
-   
-    rim = RIM3D(cell, input_layer, output_layer, niter=params['rim_iter'])
-
-    return rim
 
 
-
+    
 def build_rim_series(params):
 
     nc = params['nc']
@@ -278,33 +256,4 @@ def build_rim_series(params):
     return rim
 
 
-
-
-def build_rim_parallel(params):
-
-    nc = params['nc']
-    input_layer = Conv3D(params['input_size'], kernel_size=params['input_kernel_size'], 
-                         trainable=True, padding='SAME', 
-                         input_shape=(None, nc, nc, nc, 2), activation=params['input_activation'])
-
-    input_layer_sub = Conv3D(params['input_size'], kernel_size=params['input_kernel_size'], 
-                         trainable=True, padding='SAME', strides= [params['strides']]*3,
-                         input_shape=(None, nc, nc, nc, 2), activation=params['input_activation'])
-
-    cell1 = ConvLSTM3DCell(params['cell_size'], kernel_size=params['cell_kernel_size'], padding='SAME')
-    #cell1.build(input_shape=[None, nc, nc, nc, params['input_size']])
-
-    output_layer_up = Conv3DTranspose(1, kernel_size=params['middle_kernel_size'], 
-                         trainable=True, padding='SAME', strides=[params['strides']]*3, 
-                         activation=params['input_activation'])
-
-    cell2 = ConvLSTM3DCell(params['cell_size'], kernel_size=params['cell_kernel_size'], padding='SAME')
-
-    output_layer = Conv3D(1, kernel_size=params['output_kernel_size'], trainable=True, padding='SAME', 
-                          input_shape=(None, nc, nc, nc, params['cell_size']), activation=params['output_activation'])
-   
-    rim = RIM3D_parallel(cell1, cell2, input_layer, input_layer_sub, output_layer_up, output_layer, strides=params['strides'],
-                       niter=params['rim_iter'])
-
-    return rim
 
